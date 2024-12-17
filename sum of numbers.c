446b3483f603b438865acc4c47ca5e8029a37596
MJkattupalli <mkattupa@student.gitam.edu>
2024-12-17 14:45:35 +0530

    Create sum of numbers.c

---
+++ sum of numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a number:\n");
+    scanf("%d %d",&a,&b);
+    printf("Sum=%d",a+b);
+    return 0;
+}
+
+OUTPUT
+Enter a number:
+5
+5
+Sum=10
